722b69e60be1fb45795d7cd1540c44f0b43fa9ea
ashis <ashis@DESKTOP-3TVS1HU>
2021-07-27 23:42:57 +0530

    commit exception handling

---
+++ exceptionhandlingpractice/src/exceptionhandlingpractice/A.java
@@ -0,0 +1,56 @@
+package exceptionhandlingpractice;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.NoSuchElementException;
+
+public class A {
+	
+	
+	/*public void getdata() {
+		
+		try { 
+		
+       File f = new File("E:\\texting23.txt");
+       FileReader fr = new FileReader(f);
+		
+		}
+		catch(FileNotFoundException e )
+		{
+			
+			System.out.println("printlogs");
+			
+		}
+		
+		catch(NoSuchElementException e )
+		{
+			
+			System.out.println("printlogs");
+			System.out.println("snapshot");
+		}
+		
+		catch(Exception e )
+		{
+			
+			System.out.println("printlogs");
+			System.out.println("snapshot");
+			System.out.println("email");
+			
+		}
+		
+		finally
+		{
+			System.out.println("endtask");
+		}
+	}*/
+	
+	public void getdata(int a ) {
+		
+		if(a<20){
+			
+			throw (new NoSuchElementException());
+			
+		}
+	}
+}
---
+++ exceptionhandlingpractice/src/exceptionhandlingpractice/B.java
@@ -0,0 +1,17 @@
+package exceptionhandlingpractice;
+
+import java.io.FileNotFoundException;
+
+public class B {
+
+	
+	
+public static void main(String[] args) throws NoSuchMethodException , FileNotFoundException  {
+		
+     A obj = new A(); 
+     obj.getdata(12);
+
+		
+	}
+
+}
